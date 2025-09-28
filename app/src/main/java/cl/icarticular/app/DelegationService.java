2e763533e02a5aa58cc06dc023e24b10f6ab6c7f
cahuerta <cahuerta@gmail.com>
2025-09-28 12:37:01 -0300

    Add files via upload

---
+++ app/src/main/java/cl/icarticular/app/DelegationService.java
@@ -0,0 +1,14 @@
+package cl.icarticular.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
